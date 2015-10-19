e01903e26d7d483e7a70863abfd326e4566896c2
Cedric Beust <cedric@beust.com>
2015-10-18 17:59:23 -0700

    Adding dependencies.

--- src/com/beust/kobalt/intelli/plugin/KobaltWeigher.kt
+++ src/com/beust/kobalt/intelli/plugin/KobaltWeigher.kt
@@ -4,16 +4,17 @@ import com.intellij.codeInsight.completion.CompletionLocation
 import com.intellij.codeInsight.completion.CompletionWeigher
 import com.intellij.codeInsight.completion.JavaMethodCallElement
 import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.roots.ProjectFileIndex
+import com.intellij.openapi.roots.ProjectRootManager
 
 class KobaltWeigher : CompletionWeigher() {
     override fun weigh(element: LookupElement, location: CompletionLocation): Comparable<Nothing>? {
+        val project = location.project
         if ("Build.kt" == element.psiElement?.containingFile?.name && element is JavaMethodCallElement) {
             val annotations = element.`object`.modifierList.annotations
-            if (annotations.any { it.qualifiedName == "Directive" }) {
-                return 1
-            } else {
-                return -1
-            }
+            return if (annotations.any { it.qualifiedName == "Directive" }) 1
+                else -1
         }
         return null
     }
---
+++ src/com/beust/kobalt/intelli/plugin/TextBoxes.kt
@@ -0,0 +1,41 @@
+package com.beust.kobalt.intelli.plugin
+
+import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.roots.ModuleRootManager
+import com.intellij.openapi.roots.OrderRootType
+import com.intellij.openapi.roots.libraries.LibraryTablesRegistrar
+import com.intellij.openapi.vfs.JarFileSystem
+import com.intellij.openapi.vfs.VirtualFileManager
+
+public class TextBoxes : AnAction("Text boxes") {
+    override fun actionPerformed(event: AnActionEvent) {
+        val project = event.project
+
+        val modules = ModuleManager.getInstance(project!!).modules
+        if (modules.size() > 0) {
+            val registrar = LibraryTablesRegistrar.getInstance()
+            val libraryTable = registrar.getLibraryTable(project)
+
+            ApplicationManager.getApplication().runWriteAction {
+                val ltModel = libraryTable.modifiableModel
+                val library = ltModel.createLibrary("JCommander")
+
+                val location = "c:\\users\\cbeust\\.kobalt\\repository\\com\\beust\\jcommander\\1.48\\jcommander-1.48.jar"
+                val url = VirtualFileManager.constructUrl(JarFileSystem.PROTOCOL, location) +
+                        JarFileSystem.JAR_SEPARATOR
+                val libModel = library.modifiableModel
+                libModel.addRoot(url, OrderRootType.CLASSES)
+                libModel.commit()
+                ltModel.commit()
+
+                val moduleRootManager = ModuleRootManager.getInstance(modules[0])
+                val moduleModel = moduleRootManager.modifiableModel
+
+                moduleModel.addLibraryEntry(library)
+                moduleModel.commit()
+            }
+        }
+    }
+}
