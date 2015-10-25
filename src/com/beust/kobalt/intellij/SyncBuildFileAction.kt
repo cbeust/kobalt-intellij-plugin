f71e0f5f11d3febdac9faebfe3c4bc1256b51276
Cedric Beust <cedric@beust.com>
2015-10-25 10:28:29 -0700

    Minor fix.

--- src/com/beust/kobalt/intellij/SyncBuildFileAction.kt
+++ src/com/beust/kobalt/intellij/SyncBuildFileAction.kt
@@ -1,11 +1,12 @@
 package com.beust.kobalt.intellij
 
-import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
 
 public class SyncBuildFileAction : AnAction("Sync build file") {
     override fun actionPerformed(event: AnActionEvent) {
         event.project?.let { project ->
-            project.getComponent(KobaltProjectComponent::class.java).syncBuildFile()
+            project.getComponent(KobaltProjectComponent::class.java)?.syncBuildFile()
         }
     }
 }
