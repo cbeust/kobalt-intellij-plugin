796dc0c1b0c29fadaacc95277d924000688d2a59
Cedric Beust <cedric@beust.com>
2015-10-26 23:08:00 -0700

    Javadoc.

--- src/com/beust/kobalt/intellij/Commands.kt
+++ src/com/beust/kobalt/intellij/Commands.kt
@@ -1,5 +1,9 @@
 package com.beust.kobalt.intellij
 
+//
+// The classes in this file are mapped from the JSON coming back from the server
+//
+
 class DependencyData(val id: String, val scope: String, val path: String)
 
 class ProjectData( val name: String, val dependencies: List<DependencyData>)
