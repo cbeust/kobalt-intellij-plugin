bfc371628cf0be6cb241686a20fd89c1d28e4560
Cedric Beust <cedric@beust.com>
2015-11-17 06:19:23 -0800

    Update to 0.265 and Kotiln 2423.

--- src/com/beust/kobalt/intellij/Constants.kt
+++ src/com/beust/kobalt/intellij/Constants.kt
@@ -2,7 +2,7 @@ package com.beust.kobalt.intellij
 
 class Constants {
     companion object {
-        val MIN_KOBALT_VERSION = "0.246"
-        val DEV_MODE = false
+        val MIN_KOBALT_VERSION = "0.265"
+        val DEV_MODE = true
     }
 }
