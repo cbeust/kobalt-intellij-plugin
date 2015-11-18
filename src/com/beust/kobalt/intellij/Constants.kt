4a1da4cc62c38b28dba86b2ea1ed9be08510f4dc
Cedric Beust <cedric@beust.com>
2015-11-17 19:21:15 -0800

    1.21 / 0.270.

--- src/com/beust/kobalt/intellij/Constants.kt
+++ src/com/beust/kobalt/intellij/Constants.kt
@@ -2,7 +2,7 @@ package com.beust.kobalt.intellij
 
 class Constants {
     companion object {
-        val MIN_KOBALT_VERSION = "0.265"
+        val MIN_KOBALT_VERSION = "0.270"
         val DEV_MODE = true
     }
 }
