62ac317a5c3d2dc55359cc3a91a6f0d5ce83dd2e
Cedric Beust <cedric@beust.com>
2015-11-29 15:20:39 -0800

    Bump minimum kobalt version.

--- src/com/beust/kobalt/intellij/Constants.kt
+++ src/com/beust/kobalt/intellij/Constants.kt
@@ -2,7 +2,7 @@ package com.beust.kobalt.intellij
 
 class Constants {
     companion object {
-        val MIN_KOBALT_VERSION = "0.270"
+        val MIN_KOBALT_VERSION = "0.308"
         val DEV_MODE = false
     }
 }
