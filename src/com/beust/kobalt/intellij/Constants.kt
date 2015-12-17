c00bbaa18f79dc2b0527902f22ab693e00df3988
Cedric Beust <cedric@beust.com>
2015-12-18 02:12:15 +0400

    No DEV_MODE.

--- src/com/beust/kobalt/intellij/Constants.kt
+++ src/com/beust/kobalt/intellij/Constants.kt
@@ -5,7 +5,7 @@ class Constants {
         val MIN_KOBALT_VERSION = "0.344"
 
         /** If true, will launch kobalt found in ~/kotlin/kobalt/libs/kobalt-$DEV_VERSION.jar */
-        val DEV_MODE = true
+        val DEV_MODE = false
         val DEV_VERSION = "0.344"
 
         val BUILD_FILE = "kobalt/src/Build.kt"
