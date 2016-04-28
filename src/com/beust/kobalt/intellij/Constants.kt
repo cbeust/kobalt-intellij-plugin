f944d1fc77e33ebbb5549fb2d9974961f80950a0
Cedric Beust <cedric@beust.com>
2016-04-27 23:22:26 -0800

    1.58.

--- src/com/beust/kobalt/intellij/Constants.kt
+++ src/com/beust/kobalt/intellij/Constants.kt
@@ -2,11 +2,11 @@ package com.beust.kobalt.intellij
 
 class Constants {
     companion object {
-        val MIN_KOBALT_VERSION = "0.750"
+        val MIN_KOBALT_VERSION = "0.751"
 
         /** If true, will launch kobalt found in ~/kotlin/kobalt/libs/kobalt-$DEV_VERSION.jar */
         val DEV_MODE = false
-        val DEV_VERSION = "0.751"
+        val DEV_VERSION = "0.752"
 
         val BUILD_FILE = "kobalt/src/Build.kt"
     }
