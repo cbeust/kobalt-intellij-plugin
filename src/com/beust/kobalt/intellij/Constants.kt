6daa4f0ebfbd87ff3e440dcb14027a6f3e823974
Cedric Beust <cedric@beust.com>
2016-04-29 01:30:43 -0800

    Use 0.752.

--- src/com/beust/kobalt/intellij/Constants.kt
+++ src/com/beust/kobalt/intellij/Constants.kt
@@ -2,11 +2,11 @@ package com.beust.kobalt.intellij
 
 class Constants {
     companion object {
-        val MIN_KOBALT_VERSION = "0.751"
+        val MIN_KOBALT_VERSION = "0.752"
 
         /** If true, will launch kobalt found in ~/kotlin/kobalt/libs/kobalt-$DEV_VERSION.jar */
         val DEV_MODE = false
-        val DEV_VERSION = "0.752"
+        val DEV_VERSION = "0.753"
 
         val BUILD_FILE = "kobalt/src/Build.kt"
     }
