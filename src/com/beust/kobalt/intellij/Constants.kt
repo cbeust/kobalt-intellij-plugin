6b462512b7e640d4d18cf7e0991fd15dff713116
Cedric Beust <cedric@beust.com>
2016-05-05 23:03:39 -0800

    Bump the server version.

--- src/com/beust/kobalt/intellij/Constants.kt
+++ src/com/beust/kobalt/intellij/Constants.kt
@@ -5,13 +5,13 @@ import org.jetbrains.annotations.NonNls
 
 class Constants {
     companion object {
-        val MIN_KOBALT_VERSION = "0.762"
+        val MIN_KOBALT_VERSION = "0.764"
 
         @JvmField @NonNls val KOBALT_SYSTEM_ID = ProjectSystemId("KOBALT")
 
         /** If true, will launch kobalt found in ~/kotlin/kobalt/libs/kobalt-$DEV_VERSION.jar */
         val DEV_MODE = false
-        val DEV_VERSION = "0.763"
+        val DEV_VERSION = MIN_KOBALT_VERSION + 1
 
         val BUILD_FILE_NAME = "Build.kt"
 
