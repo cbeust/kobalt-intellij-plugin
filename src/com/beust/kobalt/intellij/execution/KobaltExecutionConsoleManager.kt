10d369a60bde72062aae5f1f70d7cc372949ba4b
Dmitry Zhuravlev <dmitry.anatolyevich.zhuravlev@gmail.com>
2016-05-26 13:41:21 +0300

    * fix: prevent open Kobalt task execution console in Gradle based project

--- src/com/beust/kobalt/intellij/execution/KobaltExecutionConsoleManager.kt
+++ src/com/beust/kobalt/intellij/execution/KobaltExecutionConsoleManager.kt
@@ -1,6 +1,7 @@
 package com.beust.kobalt.intellij.execution
 
 import com.beust.kobalt.intellij.Constants
+import com.intellij.openapi.externalSystem.model.task.ExternalSystemTask
 import com.intellij.openapi.externalSystem.service.execution.DefaultExternalSystemExecutionConsoleManager
 
 /**
@@ -9,4 +10,5 @@ import com.intellij.openapi.externalSystem.service.execution.DefaultExternalSyst
  */
 class KobaltExecutionConsoleManager : DefaultExternalSystemExecutionConsoleManager() {
     override fun getExternalSystemId() = Constants.KOBALT_SYSTEM_ID
+    override fun isApplicableFor(task: ExternalSystemTask) = task.id.projectSystemId == Constants.KOBALT_SYSTEM_ID
 }
