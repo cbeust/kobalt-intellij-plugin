a257dca4a892d8854eb812488824b7349e909773
Dmitry Zhuravlev <dmitry.anatolyevich.zhuravlev@gmail.com>
2017-04-07 17:15:27 +0300

    * possible fix for SOF. close #75

--- src/com/beust/kobalt/intellij/import/KobaltAutoImportAware.kt
+++ src/com/beust/kobalt/intellij/import/KobaltAutoImportAware.kt
@@ -5,6 +5,7 @@ import com.intellij.openapi.externalSystem.ExternalSystemAutoImportAware
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.LocalFileSystem
+import java.io.File
 
 /**
  * @author Dmitry Zhuravlev
@@ -13,13 +14,13 @@ import com.intellij.openapi.vfs.LocalFileSystem
 class KobaltAutoImportAware : ExternalSystemAutoImportAware {
     override fun getAffectedExternalProjectPath(changedFileOrDirPath: String, project: Project): String? {
         if (!changedFileOrDirPath.endsWith(Constants.BUILD_FILE_NAME)) return null
-        val file = LocalFileSystem.getInstance().refreshAndFindFileByPath(changedFileOrDirPath) ?: return null
+        val file = LocalFileSystem.getInstance().findFileByIoFile(File(changedFileOrDirPath)) ?: return null
         val possibleProjectRootPath = file.parent?.parent?.parent?.path   //we should remove "/kobalt/src/Build.kt" to get project root
-        val paths = ExternalSystemApiUtil.getManager(Constants.KOBALT_SYSTEM_ID)!!.getSettingsProvider()
+        return ExternalSystemApiUtil.getManager(Constants.KOBALT_SYSTEM_ID)!!.getSettingsProvider()
                 .`fun`(project).getLinkedProjectsSettings()
                 .map { it.externalProjectPath }
                 .filter { it == possibleProjectRootPath }
-        return if (paths.isNotEmpty()) paths[0] else null
+                .firstOrNull()
     }
 
 }
